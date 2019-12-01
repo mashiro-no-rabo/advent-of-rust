84aebab1d993f504aac357d09ac50c0836107453
LOU Xun <aquarhead@ela.build>
2019-12-01 12:01:00 +0000

    Use async-std Stream

--- examples/2019-1.rs
+++ examples/2019-1.rs
@@ -1,52 +1,48 @@
 use async_std::fs::File;
 use async_std::io::BufReader;
 use async_std::prelude::*;
-use async_std::stream::{self, IntoStream, Stream, StreamExt};
+use async_std::stream;
 use async_std::task;
 
-async fn calc_fuel(mass_str: &str) -> isize {
-  match mass_str.parse::<isize>() {
-    Ok(mass) => mass / 3 - 2,
-    Err(_) => 0,
-  }
-}
-
-async fn fuel_fuel(fuel: isize) -> isize {
-  if fuel < 9 {
+fn calc_fuel(mass: i64) -> i64 {
+  // 8 div 3 - 2 = 0, 9 div 3 - 2 = 1
+  if mass < 9 {
     0
   } else {
-    fuel / 3 - 2
+    mass / 3 - 2
   }
 }
 
 fn main() {
   task::block_on(async {
     let file = File::open("inputs/2019/1_1.txt").await.unwrap();
-    let mut lines = BufReader::new(file).lines();
-    let s = stream::from_iter(lines);
-    s.filter_map(|x| x.parse::<isize>().ok()).map(|x| fuel_fuel(x));
+    let lines: Vec<i64> = BufReader::new(file)
+      .lines()
+      .filter_map(|x| x.ok())
+      .filter_map(|x| x.parse::<i64>().ok())
+      .collect()
+      .await;
 
-    let x = s.cloned();
+    let simple_sum = stream::from_iter(lines.clone()).map(calc_fuel).sum().await;
 
-    let mut sum = 0;
-    let mut fuels = vec![];
+    println!("Simple fuel: {}", simple_sum);
 
-    while let Some(line) = lines.next().await {
-      let fuel = calc_fuel(&line.unwrap()).await;
-      sum += fuel;
-      fuels.push(fuel);
-    }
+    let real_sum = stream::from_iter(lines)
+      .map(|mut x| {
+        // Make a stream of fuel requirements for fuel
+        stream::from_fn(move || {
+          if x > 0 {
+            x = calc_fuel(x);
+            Some(x)
+          } else {
+            None
+          }
+        })
+      })
+      .flatten()
+      .sum()
+      .await;
 
-    println!("Simple fuel: {}", x.sum().await);
-
-    while let Some(fuel) = fuels.pop() {
-      let ff = fuel_fuel(fuel).await;
-      if ff > 0 {
-        sum += ff;
-        fuels.push(ff);
-      }
-    }
-
-    println!("Real fuel: {}", sum);
+    println!("Real fuel: {}", real_sum);
   })
 }
