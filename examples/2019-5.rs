2775a3305e9fa3b0f30ace012cf85c9477598a1f
LOU Xun <aquarhead@ela.build>
2019-12-05 08:26:07 +0000

    Add 2019 day 5 part 1

---
+++ examples/2019-5.rs
@@ -0,0 +1,102 @@
+use async_std::fs::File;
+use async_std::io::BufReader;
+use async_std::prelude::*;
+use async_std::task;
+use std::str;
+
+type InstParamMode = (u8, Vec<u8>);
+fn parse_inst(code: i64) -> Result<InstParamMode, ()> {
+  if code < 0 {
+    return Err(());
+  }
+
+  let inst = code % 100;
+
+  let mut param_modes = vec![0; 3];
+  let mut modes = code / 100;
+  let mut idx = 0;
+  while modes > 9 {
+    param_modes[idx] = (modes % 10) as u8;
+    modes /= 10;
+    idx += 1;
+  }
+  param_modes[idx] = modes as u8;
+
+  Ok((inst as u8, param_modes))
+}
+
+fn read_param(param: i64, mode: u8, mem: &[i64]) -> i64 {
+  match mode {
+    0 => mem[param as usize],
+    1 => param,
+    _ => unimplemented!(),
+  }
+}
+
+fn run_intcode(mem: &mut Vec<i64>, input: Vec<i64>) -> Result<Vec<i64>, ()> {
+  let mut pc = 0; // program counter
+  let mut input_idx = 0;
+  let mut outputs = vec![];
+
+  loop {
+    // parse instruction
+    let (inst, pmode) = parse_inst(mem[pc])?;
+
+    match inst {
+      99 => break,
+      1 => {
+        // plus
+        let pstart = pc + 1;
+        let op1 = read_param(mem[pstart], pmode[0], &mem);
+        let op2 = read_param(mem[pstart + 1], pmode[1], &mem);
+        let target = mem[pc + 3];
+        mem[target as usize] = op1 + op2;
+        pc += 4;
+      }
+      2 => {
+        // multiply
+        let pstart = pc + 1;
+        let op1 = read_param(mem[pstart], pmode[0], &mem);
+        let op2 = read_param(mem[pstart + 1], pmode[1], &mem);
+        let target = mem[pc + 3];
+        mem[target as usize] = op1 * op2;
+        pc += 4;
+      }
+      3 => {
+        // input
+        let target = mem[pc + 1];
+        mem[target as usize] = input[input_idx];
+        input_idx += 1;
+        pc += 2;
+      }
+      4 => {
+        // output
+        let output = read_param(mem[pc + 1], pmode[0], &mem);
+        outputs.push(output);
+        pc += 2;
+      }
+      _ => return Err(()),
+    }
+  }
+
+  Ok(outputs)
+}
+
+fn main() {
+  task::block_on(async {
+    let file = File::open("inputs/2019/5.txt").await.unwrap();
+    let mem: Vec<i64> = BufReader::new(file)
+      .split(b',')
+      .filter_map(|x| x.ok())
+      .filter_map(|x| str::from_utf8(&x).unwrap().trim().parse::<i64>().ok())
+      .collect()
+      .await;
+
+    let mut run1_mem = mem.clone();
+
+    println!(
+      "Diagnostic code: {}",
+      run_intcode(&mut run1_mem, vec![1]).unwrap().last().unwrap()
+    );
+  });
+}
