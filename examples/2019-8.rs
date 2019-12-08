e5605505e0dec6d00a43ba29f46d37303b848d0d
LOU Xun <aquarhead@ela.build>
2019-12-08 11:23:19 +0000

    Add 2019 day 8 part 2

--- examples/2019-8.rs
+++ examples/2019-8.rs
@@ -44,8 +44,38 @@ fn part1(input: &str) -> i64 {
   result
 }
 
+fn part2(input: &str) {
+  let pixels = input.bytes();
+
+  let mut image = vec![2; 25 * 6];
+
+  let mut pos = 0; // position in a layer
+  for pixel in pixels {
+    if image[pos] == 2 {
+      image[pos] = pixel - b'0';
+    }
+
+    pos += 1;
+    if pos == 25 * 6 {
+      pos = 0;
+    }
+  }
+
+  for row in 0..6 {
+    for col in 0..25 {
+      if image[row * 25 + col] > 0 {
+        print!("{}", image[row * 25 + col]);
+      } else {
+        print!(" ");
+      }
+    }
+    println!();
+  }
+}
+
 fn main() {
   let input = fs::read_to_string("inputs/2019/8.txt").unwrap();
 
   println!("Solution 1: {}", part1(input.trim()));
+  part2(input.trim());
 }
