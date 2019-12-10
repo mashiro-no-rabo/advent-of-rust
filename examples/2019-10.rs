d7ff03b1b932a592121750d198d38d48da85b52c
LOU Xun <aquarhead@ela.build>
2019-12-10 15:08:16 +0000

    Add 2019 day 10 part 2

--- examples/2019-10.rs
+++ examples/2019-10.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::fs;
 
 type Position = (i64, i64);
@@ -24,6 +25,10 @@ impl Fraction {
     Self { a, b }
   }
 
+  fn from_diff(diff: (i64, i64)) -> Self {
+    Self::from(diff.0, diff.1)
+  }
+
   fn blocks(&self, other: &Self) -> bool {
     if (self.a == 0) && (other.a == 0) {
       // when in straight line, need to ensure the direction in other axis is same
@@ -35,10 +40,45 @@ impl Fraction {
       (self.a == other.a) && (self.b == other.b)
     }
   }
+
+  fn deg(&self) -> f64 {
+    let mut ret = 90.0 - (self.b as f64).atan2(self.a as f64).to_degrees();
+
+    if ret < 0.0 {
+      ret += 360.0;
+    }
+    ret
+  }
 }
 
 fn blocks(root: &Position, a: &Position, b: &Position) -> bool {
   Fraction::from(a.0 - root.0, a.1 - root.1).blocks(&Fraction::from(b.0 - root.0, b.1 - root.1))
+}
+
+fn compare(root: &Position, a: &Position, b: &Position) -> Ordering {
+  let dega = Fraction::from(a.0 - root.0, root.1 - a.1).deg();
+  let degb = Fraction::from(b.0 - root.0, root.1 - b.1).deg();
+  if dega < degb {
+    Ordering::Less
+  } else {
+    Ordering::Greater
+  }
+}
+
+fn attempt_min(root: &Position, previous: &Position, attempt: &Position) -> Position {
+  let diff_prev = (previous.0 - root.0, previous.1 - root.1);
+  let prev_f = Fraction::from_diff(diff_prev);
+  let diff_atmp = (attempt.0 - root.0, attempt.1 - root.1);
+  let atmp_f = Fraction::from_diff(diff_atmp);
+
+  if prev_f.blocks(&atmp_f) {
+    let dist_prev = diff_prev.0.abs() + diff_prev.1.abs();
+    let dist_atmp = diff_atmp.0.abs() + diff_atmp.1.abs();
+    if dist_atmp < dist_prev {
+      return (attempt.0, attempt.1);
+    }
+  }
+  (previous.0, previous.1)
 }
 
 fn find_best_location(asteroids: &[Position]) -> usize {
@@ -68,6 +108,63 @@ fn find_best_location(asteroids: &[Position]) -> usize {
   })
 }
 
+fn find_200th(asteroids: &[Position]) -> i64 {
+  // fold to find max, start with 0
+  let (_, data) = asteroids.iter().fold((0, None), |(prev_max, prev_data), station| {
+    // take max over previous or current value
+    let detectable = asteroids
+      .iter()
+      // fold to detectable asteroids
+      .fold(vec![], |mut detectable, x| {
+        // ignore station itself
+        if x == station {
+          return detectable;
+        }
+
+        // check if any already detected asteroid blocks the attempted one
+        let blocked = detectable.iter().any(|y| blocks(station, x, y));
+        if !blocked {
+          detectable.push(x.clone())
+        }
+        detectable
+      });
+
+    if detectable.len() > prev_max {
+      let new_max = detectable.len();
+      let data = (detectable, station);
+      (new_max, Some(data))
+    } else {
+      (prev_max, prev_data)
+    }
+  });
+
+  // cheat here since we know we have > 200 asteroids detected already
+  if let Some((detected, station)) = data {
+    // count is correct (and station), but not exact asteroids
+    // need to replace the detected with the closest asteroid
+    let mut first_run: Vec<Position> = detected
+      .iter()
+      .map(|i| {
+        asteroids.iter().fold(i.clone(), |prev_closest, x| {
+          if x != station {
+            attempt_min(&station, &prev_closest, &x)
+          } else {
+            prev_closest
+          }
+        })
+      })
+      .collect();
+
+    // sort by angle
+    first_run.sort_by(|i, j| compare(station, i, j));
+
+    let (x, y) = first_run[200 - 1];
+    x * 100 + y
+  } else {
+    0
+  }
+}
+
 fn parse_asteroids(map: &str) -> Vec<Position> {
   let mut asteroids = vec![];
   for (y, line) in map.lines().enumerate() {
@@ -85,4 +182,5 @@ fn main() {
   let asteroids = parse_asteroids(&input);
 
   println!("Solution 1: {}", find_best_location(&asteroids));
+  println!("Solution 2: {}", find_200th(&asteroids));
 }
