820280aed4def5bd35a76b9dd0874190c3e0991c
LOU Xun <aquarhead@ela.build>
2019-12-15 12:52:14 +0000

    Add 2019 day 15 part 2

--- src/solutions/year2019/day15.rs
+++ src/solutions/year2019/day15.rs
@@ -70,35 +70,110 @@ fn bfs_min_steps(mem: &[i64]) -> i64 {
     steps += 1;
 
     let mut new_search = vec![];
+    let mut found = false;
 
-    let attempts = search
+    search
       .iter()
-      .flat_map(|(pos, state)| Direction::iter_all().map(move |dir| (dir.move_position(&pos), dir, state)));
-
-    for (new_pos, dir, in_state) in attempts {
-      if map.get(&new_pos).is_some() {
-        continue;
-      }
-
-      if let WaitingForInput(out_state, outputs) = in_state.run(vec![dir.to_input()]) {
-        match outputs[0] {
-          0 => {
-            map.insert(new_pos, -1);
+      .flat_map(|(pos, state)| Direction::iter_all().map(move |dir| (dir.move_position(&pos), dir, state)))
+      .for_each(|(new_pos, dir, in_state)| {
+        if map.get(&new_pos).is_none() {
+          if let WaitingForInput(out_state, outputs) = in_state.run(vec![dir.to_input()]) {
+            match outputs[0] {
+              0 => {
+                map.insert(new_pos, -1);
+              }
+              1 => {
+                map.insert(new_pos, steps);
+                new_search.push((new_pos, out_state));
+              }
+              2 => found = true,
+              _ => unimplemented!(),
+            }
           }
-          1 => {
-            map.insert(new_pos, steps);
-            new_search.push((new_pos, out_state));
-          }
-          2 => {
-            return steps;
-          }
-          _ => unimplemented!(),
         }
-      }
+      });
+
+    if found {
+      return steps;
     }
 
     search = new_search;
   }
+}
+
+fn bfs_all(mem: &[i64]) -> i64 {
+  let mut steps = 0;
+
+  let start_pos = (0, 0);
+  let mut map = HashMap::new();
+  map.insert(start_pos, 0);
+
+  let mut search = vec![];
+  let start_state = State::new_with_mem(mem);
+  search.push((start_pos, start_state));
+
+  let mut oxygen = (0, 0);
+
+  while !search.is_empty() {
+    steps += 1;
+
+    let mut new_search = vec![];
+
+    search
+      .iter()
+      .flat_map(|(pos, state)| Direction::iter_all().map(move |dir| (dir.move_position(&pos), dir, state)))
+      .for_each(|(new_pos, dir, in_state)| {
+        if map.get(&new_pos).is_none() {
+          if let WaitingForInput(out_state, outputs) = in_state.run(vec![dir.to_input()]) {
+            match outputs[0] {
+              0 => {
+                map.insert(new_pos, -1);
+              }
+              1 => {
+                map.insert(new_pos, steps);
+                new_search.push((new_pos, out_state));
+              }
+              2 => {
+                oxygen = new_pos;
+                map.insert(new_pos, steps);
+                new_search.push((new_pos, out_state));
+              }
+              _ => unimplemented!(),
+            }
+          }
+        }
+      });
+
+    search = new_search;
+  }
+
+  let mut fill = vec![];
+  fill.push(oxygen);
+  let mut minutes = 0;
+  let mut real_minutes = 0;
+
+  while !fill.is_empty() {
+    minutes += 1;
+
+    let mut new_fill = vec![];
+
+    fill
+      .iter()
+      .flat_map(|pos| Direction::iter_all().map(move |dir| (dir.move_position(&pos))))
+      .for_each(|new_pos| {
+        if let Some(x) = map.get_mut(&new_pos) {
+          if *x > -1 {
+            *x = -1;
+            new_fill.push(new_pos);
+            real_minutes = minutes;
+          }
+        }
+      });
+
+    fill = new_fill;
+  }
+
+  real_minutes
 }
 
 pub fn solution() {
@@ -106,4 +181,5 @@ pub fn solution() {
   let mem: Vec<i64> = input.split(',').map(|x| x.trim().parse::<i64>().unwrap()).collect();
 
   println!("Steps: {}", bfs_min_steps(&mem));
+  println!("Time: {}", bfs_all(&mem));
 }
