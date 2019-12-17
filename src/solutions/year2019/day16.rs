2a2989be0eb2df22d5eff69ba58db516fdf89b66
LOU Xun <aquarhead@ela.build>
2019-12-17 16:51:01 +0000

    FML 2019 day 16 part 2

--- src/solutions/year2019/day16.rs
+++ src/solutions/year2019/day16.rs
@@ -1,5 +1,5 @@
 fn fft(signal_str: &str, phases: usize) -> Vec<i64> {
-  let mut signal: Vec<i64> = signal_str.bytes().map(|x| (x - b'0') as i64).collect();
+  let mut signal: Vec<i64> = signal_str.trim().bytes().map(|x| (x - b'0') as i64).collect();
   let siglen = signal_str.len();
   for _ in 0..phases {
     let mut new_signal = vec![];
@@ -15,6 +15,60 @@ fn fft(signal_str: &str, phases: usize) -> Vec<i64> {
     signal = new_signal;
   }
   signal
+}
+
+// I though the offset need to take after 100 phases,
+// turns out it's just the input, and I have noticed
+// later part have some trick very early in the morning
+// FML!
+fn fml_fft(signal_str: &str, phases: usize) -> String {
+  let signal: Vec<u64> = signal_str.trim().bytes().map(|x| (x - b'0') as u64).collect();
+
+  // grab the offset from input
+  let offset = {
+    let mut out = 0;
+    let mut mag = 1;
+    for idx in 1..=7 {
+      out += signal[7 - idx] * mag;
+      mag *= 10;
+    }
+    out
+  };
+
+  // it should fall in the latter half
+  if (offset as usize) < (signal.len() * 5000) {
+    panic!("FML!");
+  }
+
+  // how much to skip within a signal cycle
+  let idx_offset = offset as usize % signal.len();
+  let pattern_len = (signal.len() * 10000) - (offset as usize);
+  let mut pattern = vec![1; pattern_len];
+  for _ in 0..phases - 1 {
+    // pattern[idx, phase] = pattern[idx-1, phase] + pattern[idx, phase-1]
+    for idx in 1..pattern_len {
+      pattern[idx] += pattern[idx - 1];
+      pattern[idx] %= 10;
+    }
+  }
+
+  {
+    let mut bytes = vec![];
+    for idx in 0..8 {
+      let sig = signal
+        .iter()
+        .cycle()
+        .skip(idx_offset)
+        .skip(idx)
+        .take(pattern_len - idx)
+        .zip(pattern.iter())
+        .fold(0, |acc, (s, p)| acc + (s * p))
+        % 10;
+      bytes.push(sig as u8 + b'0');
+    }
+
+    String::from_utf8(bytes).unwrap()
+  }
 }
 
 fn build_pattern(pos: usize) -> impl Iterator<Item = i64> {
@@ -35,7 +89,8 @@ fn signal_to_str(signal: &[i64], len: usize) -> String {
 pub fn solution() {
   let input = std::fs::read_to_string("inputs/2019/16.txt").unwrap();
 
-  println!("First eight: {}", signal_to_str(&fft(input.trim(), 100), 8));
+  println!("First eight: {}", signal_to_str(&fft(&input, 100), 8));
+  println!("FML eight: {}", fml_fft(&input, 100));
 }
 
 #[cfg(test)]
@@ -57,6 +112,21 @@ mod tests {
   fn test_fft_100() {
     let expected = parse_expected("24176176");
     assert_eq!(expected[..8], fft("80871224585914546619083218645595", 100)[..8]);
+  }
+
+  #[test]
+  fn test_fml_fft() {
+    assert_eq!("84462026", &fml_fft("03036732577212944063491565474664", 100));
+  }
+
+  #[test]
+  fn test_fml_fft2() {
+    assert_eq!("78725270", &fml_fft("02935109699940807407585447034323", 100));
+  }
+
+  #[test]
+  fn test_fml_fft3() {
+    assert_eq!("53553731", &fml_fft("03081770884921959731165446850517", 100));
   }
 
   fn parse_expected(input: &str) -> Vec<i64> {
