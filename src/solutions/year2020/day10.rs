d20e8548da9b2193f703b3a296099a8d5ee39695
LOU Xun <aquarhead@ela.build>
2020-12-10 10:31:48 +0000

    Add 2020 day 10 part 2

--- src/solutions/year2020/day10.rs
+++ src/solutions/year2020/day10.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 pub fn solution() {
@@ -5,15 +6,39 @@ pub fn solution() {
   let mut adapters: Vec<_> = content.lines().map(|x| x.parse::<u32>().unwrap()).collect();
   adapters.sort();
 
-  let (diff1, _, diff3, _) = adapters
-    .iter()
-    .cloned()
-    .fold((0, 0, 0, 0), |(d1, d2, d3, prev), a| match a - prev {
-      1 => (d1 + 1, d2, d3, a),
-      2 => (d1, d2 + 1, d3, a),
-      3 => (d1, d2, d3 + 1, a),
-      _ => unimplemented!(),
-    });
+  {
+    // part 1
+    let (diff1, _, diff3, _) = adapters
+      .iter()
+      .cloned()
+      .fold((0, 0, 0, 0), |(d1, d2, d3, prev), a| match a - prev {
+        1 => (d1 + 1, d2, d3, a),
+        2 => (d1, d2 + 1, d3, a),
+        3 => (d1, d2, d3 + 1, a),
+        _ => unimplemented!(),
+      });
 
-  println!("Multiply: {}", diff1 * (diff3 + 1));
+    println!("Multiply: {}", diff1 * (diff3 + 1));
+  }
+
+  {
+    // part 2
+    let mut ways = HashMap::new();
+    ways.insert(0 as i32, 1 as u64);
+    let w = adapters
+      .into_iter()
+      .map(|n| {
+        let n_1 = *(ways.get(&(n as i32 - 1)).unwrap_or(&0));
+        let n_2 = *(ways.get(&(n as i32 - 2)).unwrap_or(&0));
+        let n_3 = *(ways.get(&(n as i32 - 3)).unwrap_or(&0));
+        let n_ways = n_1 + n_2 + n_3;
+        ways.insert(n as i32, n_ways);
+
+        n_ways
+      })
+      .last()
+      .unwrap();
+
+    println!("Ways: {}", w);
+  }
 }
