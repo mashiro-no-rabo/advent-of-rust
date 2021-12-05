286e2d42b4945f764bd924d149780f3b714c25cf
LOU Xun <aquarhead@ela.build>
2021-12-05 13:48:33 +0000

    2021 day 5 part 22021 day 5 part 2

--- src/solutions/year2021/day5.rs
+++ src/solutions/year2021/day5.rs
@@ -26,6 +26,7 @@ pub fn solution() {
 
   // map of Pos to number of vents
   let mut grid = HashMap::new();
+  let mut grid1 = grid.clone();
   vents.iter().for_each(|vent| {
     if vent.start.0 == vent.end.0 {
       let a = vent.start.1.min(vent.end.1);
@@ -33,6 +34,7 @@ pub fn solution() {
 
       for y in a..=b {
         count_grid(&mut grid, (vent.start.0, y));
+        count_grid(&mut grid1, (vent.start.0, y));
       }
     } else if vent.start.1 == vent.end.1 {
       let a = vent.start.0.min(vent.end.0);
@@ -40,11 +42,34 @@ pub fn solution() {
 
       for x in a..=b {
         count_grid(&mut grid, (x, vent.start.1));
+        count_grid(&mut grid1, (x, vent.start.1));
+      }
+    } else {
+      // diagonal
+      let x_change = if vent.start.0 < vent.end.0 {
+        std::ops::Add::add
+      } else {
+        std::ops::Sub::sub
+      };
+      let y_change = if vent.start.1 < vent.end.1 {
+        std::ops::Add::add
+      } else {
+        std::ops::Sub::sub
+      };
+
+      let mut cur = vent.start;
+      loop {
+        count_grid(&mut grid, cur);
+        if cur == vent.end {
+          break;
+        }
+        cur = (x_change(cur.0, 1), y_change(cur.1, 1));
       }
     }
   });
 
-  println!("overlaps: {}", grid.iter().filter(|(_, n)| **n > 1).count());
+  println!("overlap: {}", grid1.values().filter(|n| **n > 1).count());
+  println!("all overlaps: {}", grid.values().filter(|n| **n > 1).count());
 }
 
 fn parse_pos<'a>(str_iter: &mut impl Iterator<Item = &'a str>) -> Pos {
