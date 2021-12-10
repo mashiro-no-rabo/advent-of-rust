8004e70712bcf6e3f7a49f2bbec68be06c79a133
LOU Xun <aquarhead@ela.build>
2021-12-10 10:06:51 +0000

    2021 day 10 part 2

--- src/solutions/year2021/day10.rs
+++ src/solutions/year2021/day10.rs
@@ -1,41 +1,66 @@
 use std::fs;
 
+enum Part {
+  One(u32),
+  Two(Vec<char>),
+}
+use Part::*;
+
 pub fn solution() {
   let content = fs::read_to_string("inputs/2021/10.txt").unwrap();
 
-  let p1 = content
-    .trim()
-    .lines()
-    .filter_map(|line| {
-      if let Err(corrupt) = line.chars().try_fold(Vec::new(), |mut q, c| match c {
-        ')' | ']' | '}' | '>' => {
-          let open = q.pop().unwrap();
-          if close(open) == c {
-            Ok(q)
-          } else {
-            Err(c)
-          }
-        }
-        '(' | '[' | '{' | '<' => {
-          q.push(c);
+  let results = content.trim().lines().map(|line| {
+    let fr = line.chars().try_fold(Vec::new(), |mut q, c| match c {
+      ')' | ']' | '}' | '>' => {
+        let open = q.pop().unwrap();
+        if close(open) == c {
           Ok(q)
+        } else {
+          Err(c)
         }
-        _ => unimplemented!(),
-      }) {
-        match corrupt {
-          ')' => Some(3),
-          ']' => Some(57),
-          '}' => Some(1197),
-          '>' => Some(25137),
-          _ => unimplemented!(),
-        }
-      } else {
-        None
       }
+      '(' | '[' | '{' | '<' => {
+        q.push(c);
+        Ok(q)
+      }
+      _ => unimplemented!(),
+    });
+
+    match fr {
+      Ok(v) => Two(v.into_iter().rev().map(close).collect()),
+      Err(corrupt) => match corrupt {
+        ')' => One(3),
+        ']' => One(57),
+        '}' => One(1197),
+        '>' => One(25137),
+        _ => unimplemented!(),
+      },
+    }
+  });
+
+  let p1 = results
+    .clone()
+    .filter_map(|r| match r {
+      One(n) => Some(n),
+      _ => None,
     })
     .sum::<u32>();
 
-  println!("score: {}", p1);
+  println!("syntax error score: {}", p1);
+
+  let mut p2: Vec<u64> = results
+    .filter_map(|r| match r {
+      Two(v) => Some(v.into_iter().fold(0, |mut acc, c| {
+        acc *= 5;
+        acc += acs(c);
+        acc
+      })),
+      _ => None,
+    })
+    .collect();
+  let skip = p2.len() / 2;
+  p2.sort();
+  println!("auto complete score: {}", p2.into_iter().skip(skip).next().unwrap());
 }
 
 fn close(open: char) -> char {
@@ -44,6 +69,16 @@ fn close(open: char) -> char {
     '[' => ']',
     '{' => '}',
     '<' => '>',
+    _ => unimplemented!(),
+  }
+}
+
+fn acs(c: char) -> u64 {
+  match c {
+    ')' => 1,
+    ']' => 2,
+    '}' => 3,
+    '>' => 4,
     _ => unimplemented!(),
   }
 }
