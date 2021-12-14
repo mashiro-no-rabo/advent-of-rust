d51e9ab55105d786bbd6b670f5d3b5d1dde2caef
LOU Xun <aquarhead@ela.build>
2021-12-14 16:17:02 +0000

    2021 day 14 p2

--- src/solutions/year2021/day14.rs
+++ src/solutions/year2021/day14.rs
@@ -1,5 +1,12 @@
 use itertools::{Itertools, MinMaxResult};
-use std::{collections::HashMap, fs};
+use std::{cell::RefCell, collections::HashMap, fs, rc::Rc};
+
+type Rules = HashMap<(char, char), char>;
+
+type CharCount = HashMap<char, u64>;
+type CacheKey0 = (char, char, char);
+type CacheInner = HashMap<(CacheKey0, u8), CharCount>;
+type Cache = Rc<RefCell<CacheInner>>;
 
 pub fn solution() {
   let content = fs::read_to_string("inputs/2021/14.txt").unwrap();
@@ -7,7 +14,7 @@ pub fn solution() {
   let mut lines = content.lines();
   let template = lines.next().unwrap();
 
-  let pis = lines.skip(1).fold(HashMap::new(), |mut acc, line| {
+  let rules = lines.skip(1).fold(Rules::new(), |mut acc, line| {
     let mut parts = line.split(" -> ");
     let from = {
       let mut s = parts.next().unwrap().chars();
@@ -20,26 +27,77 @@ pub fn solution() {
     acc
   });
 
-  let p1 = (0..10).fold(template.to_string(), |acc, _| {
-    let mut new_acc = String::new();
+  println!("diff 10: {}", run(template, 10, &rules));
+  println!("diff 40: {}", run(template, 40, &rules));
+}
 
-    for (a, b) in acc.chars().tuple_windows() {
-      new_acc.push(a);
-      if let Some(c) = pis.get(&(a, b)) {
-        new_acc.push(*c);
-      }
-    }
+fn run(template: &str, end: u8, rules: &Rules) -> u64 {
+  let cache = Rc::new(RefCell::new(HashMap::new()));
+  let mut result = template
+    .chars()
+    .into_iter()
+    .tuple_windows()
+    .fold(CharCount::new(), |mut acc, (a, b, c)| {
+      ins((a, b, c), 0, end, &rules, &cache).into_iter().for_each(|(k, v)| {
+        *acc.entry(k).or_insert(0) += v;
+      });
+      acc
+    });
 
-    new_acc.push(acc.chars().last().unwrap());
-    new_acc
-  });
+  // need to remove the overlapped ones
+  template
+    .chars()
+    .into_iter()
+    .skip(1)
+    .tuple_windows()
+    .for_each(|(a, b, _)| {
+      let mid_template = (a, rules.get(&(a, b)).unwrap().clone(), b);
+      ins(mid_template, 1, end, &rules, &cache)
+        .into_iter()
+        .for_each(|(k, v)| {
+          *result.get_mut(&k).unwrap() -= v;
+        });
+    });
 
-  let count = p1.chars().fold(HashMap::new(), |mut acc, c| {
-    *acc.entry(c).or_insert(0) += 1;
-    acc
-  });
-
-  if let MinMaxResult::MinMax(a, z) = count.into_iter().minmax_by_key(|a| a.1) {
-    println!("diff: {}", z.1 - a.1);
+  if let MinMaxResult::MinMax(a, z) = result.into_iter().minmax_by_key(|a| a.1) {
+    z.1 - a.1
+  } else {
+    unimplemented!()
   }
+}
+
+fn ins(template: (char, char, char), step: u8, end: u8, rules: &Rules, cache: &Cache) -> CharCount {
+  if let Some(ret) = cache.borrow().get(&(template, step)) {
+    return ret.clone();
+  }
+
+  let mut c = CharCount::new();
+
+  if step == end {
+    *c.entry(template.0).or_insert(0) += 1;
+    *c.entry(template.1).or_insert(0) += 1;
+    *c.entry(template.2).or_insert(0) += 1;
+  } else {
+    let left_template = (
+      template.0,
+      rules.get(&(template.0, template.1)).unwrap().clone(),
+      template.1,
+    );
+    let lc = ins(left_template, step + 1, end, rules, &cache.clone());
+    lc.into_iter().for_each(|(k, v)| *c.entry(k).or_insert(0) += v);
+
+    let right_template = (
+      template.1,
+      rules.get(&(template.1, template.2)).unwrap().clone(),
+      template.2,
+    );
+    let rc = ins(right_template, step + 1, end, rules, &cache.clone());
+    rc.into_iter().for_each(|(k, v)| *c.entry(k).or_insert(0) += v);
+
+    *c.get_mut(&template.1).unwrap() -= 1;
+  }
+
+  cache.borrow_mut().insert((template, step), c.clone());
+
+  c
 }
